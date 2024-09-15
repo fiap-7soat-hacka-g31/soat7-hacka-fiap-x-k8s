573bb84df66bd9db8f75900ac6a6d94803cacee2
gm50x <getuliomagela@outlook.com>
2024-09-15 10:30:14 -0300

    feat: adds private subnets

--- networking.tf
+++ networking.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "fiap_burger_vpc" {
 
 
 
-resource "aws_subnet" "fiap_burger_subnet_1a" {
+resource "aws_subnet" "fiap_burger_pub_subnet_1a" {
   vpc_id                  = aws_vpc.fiap_burger_vpc.id
   availability_zone       = "us-east-1a"
   cidr_block              = "14.0.0.0/24"
@@ -19,7 +19,7 @@ resource "aws_subnet" "fiap_burger_subnet_1a" {
   }
 }
 
-resource "aws_subnet" "fiap_burger_subnet_1b" {
+resource "aws_subnet" "fiap_burger_pub_subnet_1b" {
   vpc_id                  = aws_vpc.fiap_burger_vpc.id
   availability_zone       = "us-east-1b"
   cidr_block              = "14.0.1.0/24"
@@ -29,13 +29,23 @@ resource "aws_subnet" "fiap_burger_subnet_1b" {
   }
 }
 
-resource "aws_subnet" "fiap_burger_subnet_1c" {
+resource "aws_subnet" "fiap_burger_priv_subnet_1a" {
   vpc_id                  = aws_vpc.fiap_burger_vpc.id
-  availability_zone       = "us-east-1c"
-  cidr_block              = "14.0.2.0/24"
-  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  cidr_block              = "14.0.3.0/24"
+  map_public_ip_on_launch = false
   tags = {
-    Name = "${var.aws_cluster_name}-az-1c"
+    Name = "${var.aws_cluster_name}-az-1a"
+  }
+}
+
+resource "aws_subnet" "fiap_burger_priv_subnet_1b" {
+  vpc_id                  = aws_vpc.fiap_burger_vpc.id
+  availability_zone       = "us-east-1b"
+  cidr_block              = "14.0.4.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.aws_cluster_name}-az-1b"
   }
 }
 
@@ -44,6 +54,26 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.fiap_burger_vpc.id
   tags = {
     Name = "eks-igw"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "fiap-burger-nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.fiap_burger_pub_subnet_1a.id
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+
+  tags = {
+    Name = "fiap-burger-nat-gtw"
   }
 }
 
@@ -60,17 +90,35 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.fiap_burger_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "eks-private-route-table"
+  }
+}
+
 resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.fiap_burger_subnet_1a.id
+  subnet_id      = aws_subnet.fiap_burger_pub_subnet_1a.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.fiap_burger_subnet_1b.id
+  subnet_id      = aws_subnet.fiap_burger_pub_subnet_1b.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "public_c" {
-  subnet_id      = aws_subnet.fiap_burger_subnet_1c.id
-  route_table_id = aws_route_table.public_route_table.id
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.fiap_burger_priv_subnet_1a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.fiap_burger_priv_subnet_1b.id
+  route_table_id = aws_route_table.private_route_table.id
 }
